1df09fa939da7d9c7dc324a0fb01bb9f8e1c2225
Jesus Esteban Zuñiga <jesusestebangmail@gmail.com>
2022-05-31 21:13:28 -0500

    Primer desafio

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        h1 > The loot < /h1>
+
+        <
+        p > The loot es una tienda digital dedicada a los gamer que necesitan su equipamento de controles, consolas o videojuegos, asi como targetas de regalo, suscripciones, etc. < /p>
+
+        <
+        /div>
+    );
+}
+
+export default App;
